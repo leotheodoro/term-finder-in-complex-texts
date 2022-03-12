01aae96faa3cdbc96f980007b30f7f19559b986d
Leonardo Theodoro <leonardo@recruitrobin.com>
2022-03-12 11:50:50 -0300

    CLI working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import fs from "fs";
 import path from "path";
+import { Command } from "commander";
 import { findValueTermInComplexText } from "./services/findValueTermInComplextText";
 
+const program = new Command();
+
 try {
-  const text = fs.readFileSync(path.join(__dirname, 'mock', 'example.txt'), 'utf-8');
-  const values = findValueTermInComplexText("TEMP(K)", text);
-  console.log(values);
+  program
+    .command('find')
+    .description('Find a term in a complex text')
+    .argument('<path>', 'Path to the complex text')
+    .option('--term <term>', 'term to find')
+    .action((filePath, options) => {
+      if (!options.term)
+        return console.log('You need to insert a value into the --term flag');
+
+      const text = fs.readFileSync(filePath, 'utf-8');
+      const values = findValueTermInComplexText(options.term, text);
+
+      console.log(values);
+    });
+
+  program.parse();
 } catch (error) {
   console.log(error);
 }
